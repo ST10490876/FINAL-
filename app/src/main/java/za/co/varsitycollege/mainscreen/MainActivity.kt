b1f4b7505b9aa83ccbea2e4be8410ecbc2a85db4
ST10490876 <ST10490876@vcconnect.edu.za>
2025-06-19 11:53:09 +0200

    INITIAL COMMIT

--- app/src/main/java/za/co/varsitycollege/mainscreen/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/mainscreen/MainActivity.kt
@@ -1,16 +1,51 @@
 package za.co.varsitycollege.mainscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //
+    companion object {
+        val songs = arrayOf("One time","Love me now","Favourite girl","Hello")
+        val artists = arrayOf("Justin bieber","John legend","Justin bieber","Adele")
+        val ratings = arrayOf("5,3,4,5")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //
+        val addToPlaylistButton = findViewById<Button>(R.id.buttonAdd)
+        addToPlaylistButton.setOnClickListener {
+
+        }
+
+        //
+        val viewPlaylistButton = findViewById<Button>(R.id.buttonViewPlaylist)
+        viewPlaylistButton.setOnClickListener {
+            val intent = Intent(this, SecondScreen::class.java)
+            startActivity(intent)
+        }
+
+        //
+        val exitButton = findViewById<Button>(R.id.buttonExit)
+        exitButton.setOnClickListener{
+            finishAffinity()
+        }
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/za/co/varsitycollege/mainscreen/SecondScreen.kt
+++ app/src/main/java/za/co/varsitycollege/mainscreen/SecondScreen.kt
@@ -1,16 +1,56 @@
 package za.co.varsitycollege.mainscreen
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondScreen : AppCompatActivity() {
+
+    private lateinit var playlistTextView: TextView
+    private lateinit var ratingTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second_screen)
+
+        playlistTextView = findViewById(R.id.textPlaylist)
+        ratingTextView = findViewById(R.id.textRating)
+
+        //
+        val songsButton = findViewById<Button>(R.id.buttonSongs)
+        songsButton.setOnClickListener {
+            val songList = StringBuilder()
+            for (i in MainActivity.songs.indices) {
+                songList.append("Song: ${MainActivity.songs[i]}\n")
+                songList.append("Artist: ${MainActivity.artists[i]}\n")
+                songList.append("Rating: ${MainActivity.artists[i]}\n")
+
+            }
+            playlistTextView.text = songList.toString()
+        }
+
+        //
+        val ratingButton = findViewById<Button>(R.id.buttonRating)
+        ratingButton.setOnClickListener {
+            val total = MainActivity.ratings.sum()
+            val avarage = total.toString()
+            MainActivity.ratings.size
+            ratingTextView.text = "Average rating: %.3f".format(avarage)
+        }
+
+        //
+        val returnButton = findViewById<Button>(R.id.buttonReturn)
+        returnButton.setOnClickListener {
+            finish()
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,3 +58,14 @@ class SecondScreen : AppCompatActivity() {
         }
     }
 }
+
+private fun Any.sum() {
+
+}
+
+
+
+
+
+
+
